9b9d6ae6a4995fbbbb34dc0268462d8ec18b489a
Grant Morris <grant.morris.dev@gmail.com>
2020-10-04 13:42:57 -0400

    Implement wav_file_16_create(), change chunkId & format to uint32_t

--- src/main.c
+++ src/main.c
@@ -48,10 +48,10 @@ typedef struct {
     WavFmtChunk fmtChunk;
     WavDataChunk dataChunk;
     int16_t* data;
-} WavFile16;
+} WavFileInt;
 
 
-WavFile16* wav_file_create(WavFileHeader header, WavFmtChunk fmtChunk, WavDataChunk dataChunk, uint8_t* audioData) {
+WavFileInt* wav_file_create(WavFileHeader header, WavFmtChunk fmtChunk, WavDataChunk dataChunk, uint8_t* audioData) {
 
 }
 
@@ -79,26 +79,24 @@ uint32_t reverse_endianness_uint32_t ( uint32_t value) {
 }
 
 
-void wav_file_write() {}
-
-
 WavFileHeader wav_file_header_create(uint32_t fileSize) {
     // add support for big endian {RIFX} (pass enum as parameter)
-    uint8_t chunkId[] = {'R','I','F','F'};
-    uint32_t chunkSize = 0;
-    uint8_t format[] = {'W','A','V','E'};
+    uint32_t chunkId = 0x52494646; //RIFF in ASCII (big endian)
+    uint32_t chunkSize = 0; 
+    uint32_t format = 0x57415645; // WAVE in ASCII (big endian)
 
-    chunkSize = is_uint32_t_big_endian ? chunkSize: reverse_endianness_uint32_t(chunkSize);
+    chunkSize = is_uint32_t_big_endian ? chunkSize : reverse_endianness_uint32_t(chunkSize);
 
-    WavFileHeader header = {*chunkId, chunkSize, *format};
+    WavFileHeader header = {chunkId, chunkSize, format};
 
     return header;
 }
 
-// later, just pass in pointer to audio buffer
-WavFmtChunk wav_format_chunk_create(uint32_t subChunkSize, uint16_t audioFormat, uint16_t numChannels, uint32_t sampleRate, uint16_t bitDepth) {
+
+WavFmtChunk wav_format_chunk_create(uint16_t numChannels, uint32_t sampleRate, uint16_t bitDepth) {
     uint32_t subChunkId = 0x666d7420; // fmt  in ASCII
-    audioFormat = 1; // endianness
+    uint16_t audioFormat = 1; // endianness
+    uint16_t subChunkSize = 16; // 16 for PCM
 
     const uint32_t byteRate = (uint32_t) (sampleRate * numChannels * bitDepth / 8);
     const uint16_t blockAlign = (uint16_t) (numChannels * bitDepth / 8);
@@ -117,7 +115,7 @@ WavFmtChunk wav_format_chunk_create(uint32_t subChunkSize, uint16_t audioFormat,
     return fmtChunk;
 }
 
-// later, just pass in pointer to audio buffer
+
 WavDataChunk wav_data_chunk_create(uint64_t samples, uint16_t bitDepth) {
 
     uint32_t subChunk2Id = 0x64617461;
@@ -171,11 +169,47 @@ void audio_buffer_16_print(AudioBuffer16* buffer) {
 }
 
 
+WavFileInt* wav_file_16_create(AudioBuffer16* buffer, uint16_t numChannels) {
+    uint64_t sampleRate = buffer->frequency;
+    uint16_t bitDepth = 16;
+    uint8_t bytesPerBit = 8;
+
+    uint32_t subChunk2Size = buffer->samples * numChannels * bitDepth / bytesPerBit;
+
+    WavFmtChunk fmtChunk = wav_format_chunk_create(numChannels, sampleRate, bitDepth);
+
+    WavDataChunk dataChunk = wav_data_chunk_create(sampleRate, bitDepth);
+
+    WavFileHeader headerChunk = wav_file_header_create(subChunk2Size);
+
+    WavFileInt* wavFilePtr = calloc(1, sizeof(WavFileInt));
+
+    WavFileInt wavFile = {
+        headerChunk,
+        fmtChunk,
+        dataChunk
+    };
+
+    wavFilePtr = &wavFile;
+    return wavFilePtr;
+}
+
+
+void wav_file_write(WavFileInt* file) {
+   // only variable length is the data 
+}
+
 int main() {
+    uint64_t numSamples = 24000;
+    uint16_t sampleRate = 48000;
+    uint16_t noteFrequency = 440.0;
+
     AudioBuffer16* testBuffer = calloc(1, sizeof(AudioBuffer16));
 
-    testBuffer = audio_buffer_16_create(48000, 32);
-    testBuffer = audio_buffer_16_sin( 440.0, testBuffer);
+    testBuffer = audio_buffer_16_create(sampleRate, numSamples);
+    testBuffer = audio_buffer_16_sin(noteFrequency, testBuffer);
+    
+    WavFileInt* wavFile = wav_file_16_create(testBuffer, 2);
 
 
     audio_buffer_16_print(testBuffer);
